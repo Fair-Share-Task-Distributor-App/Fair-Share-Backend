accf071201d954b6b8350711fe2cd74cd156caed
bearismybrother <thetwai123456789@gmail.com>
2026-04-12 14:10:56 -0700

    AppHost now runs api

--- aspire/Fair-Share.AppHost/Program.cs
+++ aspire/Fair-Share.AppHost/Program.cs
@@ -1,3 +1,14 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+// Add database resource
+var postgres = builder.AddConnectionString("DefaultConnection");
+
+// Add API service
+var api = builder
+    .AddProject<Projects.Fair_Share_Api>("api")
+    .WithReference(postgres)
+    .WaitFor(postgres);
 
 builder.Build().Run();
